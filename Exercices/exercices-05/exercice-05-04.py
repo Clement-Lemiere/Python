76981aea535db2891f4755c34aa3c48816d05e83
Clement_Lemière <clement.lemiere.cl@gmail.com>
2023-06-12 14:20:15 +0200

    Commit exercice 5 complete

--- Exercices/exercices-05/exercice-05-04.py
+++ Exercices/exercices-05/exercice-05-04.py
@@ -8,4 +8,5 @@ Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
 
 # réponse 5.4
-
+Text2=my_text.split(' ')
+print(Text2[5])
--- Exercices/exercices-05/exercice-05-05.py
+++ Exercices/exercices-05/exercice-05-05.py
@@ -7,4 +7,5 @@ Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
 
 # réponse 5.5
-
+line_number= my_text.count('\n')
+print(line_number)
