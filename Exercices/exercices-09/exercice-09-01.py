2a51035a5853bf5c47679a723579b400e716cc28
Clement_Lemière <clement.lemiere.cl@gmail.com>
2023-06-20 11:24:50 +0200

    Exercice 9 completed

---
+++ Exercices/exercices-09/exercice-09-01.py
@@ -0,0 +1,16 @@
+# exo 9.1
+# Créez un dictionnaire nommé `my_dict` associant :
+# - une clé alphanumérique et un nombre entier
+# - une clé alphanumérique et un nombre à virgule flottante
+# - une clé alphanumérique et une chaîne de caractères
+# - une clé alphanumérique et un booléen
+# Puis affichez le résultat avec un simple `print()`
+
+# réponse 9.1
+my_dict = {
+    "en" : 95,
+    "fl" : 3.14,
+    "st" : "lorem",
+    "bo" : True
+}
+print(my_dict)
---
+++ Exercices/exercices-09/exercice-09-05.py
@@ -0,0 +1,16 @@
+# exo 9.5
+# Supprimez du dictionnaire la clé `foo`
+# Puis affichez le résultat avec un simple `print()`
+
+my_dict = {
+    'foo': 42,
+    'bar': 3.14,
+    'baz': 'lorem ipsum',
+    'lorem': True
+}
+
+# réponse 9.5
+print(my_dict)
+if 'foo' in my_dict:
+    del my_dict['foo']
+print(my_dict)
